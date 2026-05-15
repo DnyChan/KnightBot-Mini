c4178c44b3b961ab4eb761d74d14f6f045a42352
DnyChan <109867667+DnyChan@users.noreply.github.com>
2026-05-16 00:25:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,11 +4,11 @@
 
 module.exports = {
     // Bot Owner Configuration
-    ownerNumber: ['91xxxxxxxxxxx','917023951514'], // Add your number without + or spaces (e.g., 919876543210)
-    ownerName: ['Knight Bot Mini', 'Professor'], // Owner names corresponding to ownerNumber array
+    ownerNumber: ['6285788390394','62887437195264'], // Add your number without + or spaces (e.g., 919876543210)
+    ownerName: ['Scarlatte', 'Zoltraak'], // Owner names corresponding to ownerNumber array
     
     // Bot Configuration
-    botName: 'Knight Bot Mini',
+    botName: 'Zoltraak Bot Mini',
     prefix: '.',
     sessionName: 'session',
     sessionID: process.env.SESSION_ID || '',
@@ -16,7 +16,7 @@ module.exports = {
     updateZipUrl: 'https://github.com/mruniquehacker/KnightBot-Mini/archive/refs/heads/main.zip', // URL to latest code zip for .update command
     
     // Sticker Configuration
-    packname: 'Knight Bot Mini',
+    packname: 'Zoltraak Bot Mini',
     
     // Bot Behavior
     selfMode: false, // Private mode - only owner can use commands
